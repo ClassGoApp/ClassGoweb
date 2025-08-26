829d041b762142c336e5b20019e70233d02a818b
Carlos <carlosenriquetorrez26@gmail.com>
2025-08-26 16:18:36 -0400

    cambios menores

--- resources/views/livewire/reserva.blade.php
+++ resources/views/livewire/reserva.blade.php
@@ -118,7 +118,7 @@
                     <!--COMPROBANTE-->
                     <div>
                         <label class="input-label">Comprobante de pago</label>
-                        <label for="comprobante" class="file-input-label">
+                        {{-- <label for="comprobante" class="file-input-label">
                             <svg xmlns="http://www.w3.org/2000/svg" class="upload-icon" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM6.293 6.707a1 1 0 010-1.414l3-3a1 1 0 011.414 0l3 3a1 1 0 01-1.414 1.414L11 5.414V13a1 1 0 11-2 0V5.414L7.707 6.707a1 1 0 01-1.414 0z" clip-rule="evenodd" />
                             </svg>
@@ -132,7 +132,20 @@
                         @if($paymentReceipt)
                             <div class="file-name-display">{{ $paymentReceipt->getClientOriginalName() }}</div>
                         @endif
-                        @error('paymentReceipt') <span class="form-error">{{ $message }}</span> @enderror
+                        @error('paymentReceipt') <span class="form-error">{{ $message }}</span> @enderror --}}
+
+                        <label for="paymentProof" class="input-label">
+                            <input type="file" id="comprobante" wire:model="paymentReceipt" class="file-upload-content">
+
+                            <div class="file-upload-content">
+                                <span class="file-upload-icon">📄</span>
+                                @if($paymentReceipt)
+                                    <p id="fileUploadText" class="file-upload-text">{{ $paymentReceipt->getClientOriginalName() }}</p>
+                                @else
+                                    <p id="fileUploadText" class="file-upload-text">Subir archivo</p>
+                                @endif
+                            </div>
+                        </label>
                     </div>
 
                     <!--Materias-->
