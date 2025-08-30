2fe4eec2027016605ff5e5d89bf6eacafe88920c
alejandro <78042869ale@gmail.com>
2025-08-30 08:10:27 -0400

    cupon_no_carga_no_logueado_vista_perfil_tutor(reserva)

--- app/Livewire/Reserva.php
+++ app/Livewire/Reserva.php
@@ -70,7 +70,14 @@ class Reserva extends Component
         $this->isAugustPromotion = $this->currentDate->month === 8; // ✅ Verificar si es agosto
 
         $this->cuponservice = $cuponservice;  // OK
-        $this->cuponesUsuario = $this->cuponservice->todosLosCupones(auth()->user());
+
+        if (auth()->check()) {
+            $this->cuponesUsuario = $this->cuponservice->todosLosCupones(auth()->user());
+        } else {
+            $this->cuponesUsuario = [];
+            // Opcional: puedes redirigir o mostrar un mensaje
+            // session()->flash('error', 'Debes iniciar sesión para ver tus cupones.');
+        }
 
         $this->loadMonthData();
         $this->materiasTutor = UserSubject::where("user_id", $this->tutorId)->get();
@@ -287,7 +294,7 @@ class Reserva extends Component
         $isAugustPromotion = $this->currentDate->month === 8;
 
 
-        if ($isAugustPromotion || (!empty($this->cuponCode) )) {
+        if ($isAugustPromotion || (!empty($this->cuponCode))) {
             $this->validate([
                 'selectedSubject' => 'required',
             ]);
@@ -306,12 +313,12 @@ class Reserva extends Component
             $sessionFee = 15;
             $estudianteId = auth()->user()->id;
             // 2.1. registra que ya se uso el cupon en esta session
-           if (!empty($this->cuponCode)) {
+            if (!empty($this->cuponCode)) {
                 if ($service->porcentajeCupon($this->cuponCode) == 100) {
                     $sessionFee = 0;
                 }
                 $service->cuponCanjeado($this->cuponCode, auth()->user());
-                 $this->cuponesUsuario = $service->todosLosCupones(auth()->user());
+                $this->cuponesUsuario = $service->todosLosCupones(auth()->user());
             }
 
             $fechaCompleta = $this->currentDate->copy()
