e8f3917a117d3f9ae191939d38759fc167cb1547
ronaldflores2004 <159477681+ronaldflores2004@users.noreply.github.com>
2025-11-10 22:56:47 -0400

    comenzando

---
+++ bootstrap/cache/events.php
@@ -0,0 +1,16 @@
+<?php return array (
+  'App\\Providers\\EventServiceProvider' => 
+  array (
+    'Illuminate\\Auth\\Events\\Registered' => 
+    array (
+      0 => 'Illuminate\\Auth\\Listeners\\SendEmailVerificationNotification',
+    ),
+    'Amentotech\\LaraGuppy\\Events\\GuppyChatPrivateEvent' => 
+    array (
+      0 => 'App\\Listeners\\MessageReceivedListener',
+    ),
+  ),
+  'Illuminate\\Foundation\\Support\\Providers\\EventServiceProvider' => 
+  array (
+  ),
+);
