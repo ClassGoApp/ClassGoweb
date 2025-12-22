5cad4c637dcedb2150dcbdd32817d5a7b3768b35
Kevin <kaballesteros@incos-santacruz.edu.bo>
2025-12-22 17:23:57 -0400

    traducciones

--- resources/views/vistas/view/pages/components/home/nuestra-app.blade.php
+++ resources/views/vistas/view/pages/components/home/nuestra-app.blade.php
@@ -10,38 +10,38 @@
 
                 <!-- Lista de características -->
                 <ul class="list-app">
-                <li>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
-                    </svg>
-                    <span data-translate="acceso"></span>
-                </li>
-                <li>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
-                    </svg>
-                    <span data-translate="tutores_expertos"></span>
-                </li>
-                <li>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
-                    </svg>
-                    <span data-translate="tarifas_asequibles"></span>
-                </li>
+                    <li>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
+                        </svg>
+                        <span data-translate="acceso"></span>
+                    </li>
+                    <li>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
+                        </svg>
+                        <span data-translate="tutores_expertos"></span>
+                    </li>
+                    <li>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
+                        </svg>
+                        <span data-translate="tarifas_asequibles"></span>
+                    </li>
                 </ul>
                 <!-- Botón -->
                 <div>
-                    <a href="https://play.google.com/store/apps/details?id=com.neurasoft.classgo" target="_blank" class="btn-app"> Descargar Ahora </a> 
+                    <a href="https://play.google.com/store/apps/details?id=com.neurasoft.classgo" target="_blank" class="btn-app"><span data-translate="descargar_ahora"></span></a>
                 </div>
-            
+
             </div>
         </div>
 
-        <!-- Columna Derecha: Imagen -->    
+        <!-- Columna Derecha: Imagen -->
         <div class="image-app">
             <img
                 src="{{ asset('images/home/tugo-cel-optimizado.webp')}}"
-                alt="ClassGo"onerror="this.onerror=null;this.src='https://placehold.co/400x800/023047/FFFFFF?text=App';">
+                alt="ClassGo" onerror="this.onerror=null;this.src='https://placehold.co/400x800/023047/FFFFFF?text=App';">
         </div>
     </div>
 
