fc14a0ad2eff776d7d6114d2b53289267a9c8918
Carlos <carlsenrmt26@gmail.com>
2026-01-05 16:54:05 -0400

    Cambios de home y mejora en buscar tutor mobile

--- resources/views/vistas/view/pages/components/home/buscar-tutor.blade.php
+++ resources/views/vistas/view/pages/components/home/buscar-tutor.blade.php
@@ -1,11 +1,11 @@
-<h1 class="header-main__title"><span data-translate="buscar_tutor_txt1"></span></h1>
-<p class="header-main__subtitle_ligth" data-translate="buscar_tutor_txt2"></p>
-<div class="buscar-tutor-wrapper">
+<h1 class="header-main__title fade-up" ><span data-translate="buscar_tutor_txt1"></span></h1>
+<p class="header-main__subtitle_ligth fade-up" data-translate="buscar_tutor_txt2"></p>
+<div class="buscar-tutor-wrapper fade-up">
     @livewire('buscar-tutor')
 </div>
 
 
-<p class="header-main__subtitle_ligth" style="padding-top: 1rem;" id="texto" data-translate="buscar_tutor_txt3"></p>
+<p class="header-main__subtitle_ligth fade-up" style="padding-top: 1rem;" id="texto" data-translate="buscar_tutor_txt3"></p>
 <script>
     const textoPlaceholder = document.querySelector('#texto');
 
