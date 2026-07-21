2504ad46dd1475525e50f69b14119e0fd89b85ab
Fournext <sebastianzc04@gmail.com>
2026-07-21 16:08:17 -0400

    fix: bloques de 5 min no de 10

--- resources/views/vistas/view/pages/solicitud-tutor.blade.php
+++ resources/views/vistas/view/pages/solicitud-tutor.blade.php
@@ -809,11 +809,17 @@
                 <span style="font-weight:700; color:#023047;">:</span>
                 <select id="clock_minute" class="clock-select">
                     <option value="00">00</option>
+                    <option value="05">05</option>
                     <option value="10">10</option>
+                    <option value="15">15</option>
                     <option value="20">20</option>
+                    <option value="25">25</option>
                     <option value="30">30</option>
+                    <option value="35">35</option>
                     <option value="40">40</option>
+                    <option value="45">45</option>
                     <option value="50">50</option>
+                    <option value="55">55</option>
                 </select>
                 <div class="ampm-toggle">
                     <button id="ampm_am" class="ampm-btn active" onclick="setAmPm('AM')">AM</button>
